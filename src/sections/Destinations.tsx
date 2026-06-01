2680acbfeeecfe205e69932eeda76bc2490fa1b9
rupp <fuerfalien@gmail.com>
2026-06-01 13:44:33 +0700

    reduce numCopies from AhsanTV Peduli to fix annoying things

--- src/sections/Destinations.tsx
+++ src/sections/Destinations.tsx
@@ -235,7 +235,7 @@ export default function Destinations() {
         texts={['AhsanTV', 'Peduli']}
         velocity={100}
         className="custom-scroll-text"
-        numCopies={12}
+        numCopies={6}
         damping={50}
         stiffness={100}
       />
